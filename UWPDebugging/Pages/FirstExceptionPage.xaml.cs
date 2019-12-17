deeb0002ba02c10e2ab8ea2351f46e040ea51cdb
Freist Li <freistli@outlook.com>
2019-12-17 21:28:32 +0800

    commit merge result

--- UWPDebugging/Pages/FirstExceptionPage.xaml.cs
+++ UWPDebugging/Pages/FirstExceptionPage.xaml.cs
@@ -53,7 +53,7 @@ namespace UWPDebugging.Pages
             _mediaPlayer = new MediaPlayer();
             _mediaPlayer.Source = MediaSource.CreateFromUri(manifestUri);
             _mediaPlayer.Play();
-            StorageFile.GetFileFromApplicationUriAsync(uri);
+           
             ;
         }
     }
